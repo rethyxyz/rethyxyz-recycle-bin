a97c5d8315ceb004861cde0890c0a404a584610e
Brody Rethy <brodyrethy@gmail.com>
2021-09-15 23:34:39 -0300

    Perfection. Done for a while.

--- rb.sh
+++ rb.sh
@@ -2,30 +2,31 @@
 #
 # By: Brody Rethy
 # Website: https://rethy.xyz
+# GitHub: https://github.com/rethyxyz
 #
 # Name: rb.sh
 #
 # Summary:
-# A simple script to imitate the Windows recycle bin on Unix.
+# A simple script to imitate the Windows recycle bin system on Unix-like
+# systems.
+
 #
-
-#+++++++#
-# TODOs #
-#+++++++#
-
+# TODOs
+#
 # TODO: Implement a form of file compression.
+#
+# TODO: Check if $TRASH_BIN variable in argument list. if so, quit, and ask the
+# user why in the world they are trying to move the trash bin to the trash bin.
 
-#+++++++++++#
-# Variables #
-#+++++++++++#
-
+#
+# Variables
+#
 # This can be changed to anything.
-TRASH_BIN="$HOME/.Trash"
+TRASH_BIN="$HOME/.trash"
 
-#+++++++++++#
-# Functions #
-#+++++++++++#
-
+#
+# Functions
+#
 display_help() {
     printf "%s [ITEM] [ITEM 2] [ITEM 3] ...\n\n" "$0"
     printf "List the ITEM(s) you want to remove.\n"
@@ -40,53 +41,58 @@ handle_item_exists() {
         NEW_ITEM="${ITEM_PREFIX}_${ITEM}"
     done
 
-    mv "$ITEM" "$NEW_ITEM" || sudo mv "$ITEM" "$NEW_ITEM"
+    mv "$ITEM" "$NEW_ITEM" \
+    || sudo mv "$ITEM" "$NEW_ITEM"
 
     ITEM="$NEW_ITEM"
 }
 
-#++++++#
-# Main #
-#++++++#
-
-# If not args given, printf and exit.
+#
+# Main
+#
+# If no arguments given, alert and exit.
 [ $# -eq 0 ] && { printf "No filename(s) given\n"; display_help; exit 1; }
 
-# Else if an arg matches, call display_help.
-if [ "$*" = "--help" ] || [ "$*" = "-h" ]; then display_help; exit 0; fi
+# If arg matches, call display_help().
+[ "$*" = "--help" ] || [ "$*" = "-h" ] && { display_help; exit 0; }
 
-# If $TRASH_BIN directory doesn't exists, create it.
-[ ! -d "$TRASH_BIN" ] && mkdir -p "$TRASH_BIN"
+# If $TRASH_BIN directory doesn't exists, alert and create it.
+[ ! -d "$TRASH_BIN" ] && { \
+    mkdir -p "$TRASH_BIN"
+    printf "Created trash bin at \"%s\".\n" "$TRASH_BIN"
+}
 
-# Main program loop. Loops through arguments given.
+# MAIN LOOP.
 for ITEM in "$@"; do
-    # Pass on current array index if item doesn't exist.
+    # If item doesn't exist, alert and pass.
     [ ! -e "$ITEM" ] && {
-        printf "Item \"%s\" does not exist.\n" "$ITEM";
-        continue;
+        printf "Item \"%s\" does not exist.\n" "$ITEM"
+        continue
     }
 
-    # If link, remove.
+    # If link, alert and remove.
     if [ -L "$ITEM" ]; then
         {
-            rm -rf "$ITEM" && printf "Removed link \"%s\"\n." "$ITEM"
+            rm -rf "$ITEM" \
+            && printf "Removed link \"%s\"\n." "$ITEM"
         } || {
             sudo rm -rf "$ITEM" \
             && printf "Removed link \"%s\" using sudo.\n" "$ITEM"
         }
-    # If ITEM empty, remove.
+    # If ITEM empty, alert and remove.
     elif [ ! -s "$ITEM" ]; then
         {
-            rm -rf "$ITEM" && printf "Removed empty file \"%s\".\n" "$ITEM"
+            rm -rf "$ITEM" \
+            && printf "Removed empty file \"%s\".\n" "$ITEM"
         } || {
             sudo rm -rf "$ITEM" \
             && printf "Removed empty file \"%s\" using sudo.\n" "$ITEM"
         }
     else
-        # If ITEM exists in recycle bin, rename until no match.
+        # If ITEM exists in recycle bin, rename until no match found.
         [ -e "$TRASH_BIN/$ITEM" ] && handle_item_exists
 
-        # Move file to trash.
+        # Move item file to trash.
         {
             mv "$ITEM" "$TRASH_BIN/" \
             && printf "Item \"%s\" to \"%s\"\n" "$ITEM" "$TRASH_BIN"
